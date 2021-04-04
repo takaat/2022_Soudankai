7bdac59ee7b6a686338ef31a4da77b274c71ef94
mana.u <great8life@gmail.com>
2021-04-04 15:28:36 +0900

    Initial Commit

---
+++ MeigenSupplement/MeigenSupplementApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeigenSupplementApp.swift
+//  MeigenSupplement
+//
+//  Created by 上田学 on 2021/04/04.
+//
+
+import SwiftUI
+
+@main
+struct MeigenSupplementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
