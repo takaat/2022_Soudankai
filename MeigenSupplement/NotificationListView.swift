1a5f2b3f9fd02f85c9f077ab4939cf980a8e3fdf
mana.u <great8life@gmail.com>
2021-04-11 23:48:03 +0900

    20210411

---
+++ MeigenSupplement/NotificationListView.swift
@@ -0,0 +1,36 @@
+////
+////  NotificationListView.swift
+////  MeigenSupplement
+////
+////  Created by 上田学 on 2021/04/11.
+////
+//
+//import SwiftUI
+//
+//struct NotificationListView: View {
+//    let center = UNUserNotificationCenter.current()
+//    var body: some View {
+//
+//    }
+//}
+//
+//struct NotificationListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NotificationListView()
+//    }
+//}
+//
+////struct PendingNotification/*: Identifiable*/ {
+////
+////
+////    var unNotificationRequests: [([UNNotificationRequest]) -> Void] = []
+////
+////    mutating func getPendingNotification(){
+////
+////        center.getPendingNotificationRequests(completionHandler:){
+////            unNotificationRequests.append(completionHandler)
+////        }
+////
+////
+////    }
+////}
